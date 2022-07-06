d4a8b5fb80d8930958b2e85db284d6eea899e914
AyjuYaas <105471543+AyjuYaas@users.noreply.github.com>
2022-07-06 22:09:48 +0545

    Add files via upload

--- Main_screen.c
+++ Main_screen.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 
 void title_screen();
+void delay(float);
 
 int main(){
 
@@ -14,12 +17,12 @@ void title_screen(){
 
     system("cls");
     int choice;
-    printf ("\t\t\t\t\t\t WELCOME TO *** LOGIN PAGE\n\n");
-    printf ("\t\t\t\t\t Please Choose from the following options\n\n");
-    printf ("\t\t\t\t\t\t1 - Teacher Login\n");
-    printf ("\t\t\t\t\t\t2 - Student Login\n");
-    printf ("\t\t\t\t\t\t3 - Department login\n");
-    printf ("\t\t\t\t\t\tChoose: ");
+    printf ("\tWELCOME TO *** LOGIN PAGE\n\n");
+    printf ("Please Choose from the following options\n\n");
+    printf ("\t1 - Teacher Login\n");
+    printf ("\t2 - Student Login\n");
+    printf ("\t3 - Department login\n");
+    printf ("\tChoose: ");
     scanf ("%d", &choice);
 
     switch (choice) {
@@ -45,4 +48,19 @@ void title_screen(){
             title_screen();
     }
 
+}
+
+void delay(float sec) 
+{
+    // Converting time into milli_seconds
+    int milli_seconds = 1000 * sec;
+  
+    // Storing start time
+    clock_t start_time = clock();
+  
+    // looping till required time is not achieved
+    while (clock() < start_time + milli_seconds)
+        ;
+
+    /* This Piece of code Was Taken from stackoverflow.com */
 }
