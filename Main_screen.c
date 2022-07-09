a0f8de5a96031cc3e3eb507759d77aa5c0f98306
AyjuYaas <105471543+AyjuYaas@users.noreply.github.com>
2022-07-09 21:55:55 +0545

    Add files via upload

--- Main_screen.c
+++ Main_screen.c
@@ -6,6 +6,17 @@
 void title_screen();
 void delay(float);
 void dep_reg_screen();
+void department_screen();
+
+struct {
+    char un[10];
+    char pw[15];
+    char dname[20];
+    int dobm, dobd, doby;
+    int em, ed, ey;
+    char uni[40];
+    char hod[20];
+}dep, depch, stu, stuch, tea, teach;
 
 int main(){
 
@@ -37,7 +48,7 @@ void title_screen(){
             break;
 
         case 3:
-            //department_screen();
+            department_screen();
             break;
 
         case 32715:
@@ -54,17 +65,18 @@ void title_screen(){
 
 void dep_reg_screen(){
     int i, choose;
-    char username[15], password[15], d_name[15], d_uni[10], d_hod[15];
-    int e_m, e_y, e_d;
 
     system("cls");
+  /** LOADING SCREEN **/
     printf("Restricted Area Loading");
-    for(i=0; i<5; i++){
+    for(i=0; i<3; i++){
         printf(".");
         delay(0.5);
     }
     system("cls");
 
+  /** CHOOSING SCREEN **/
+    dep_create_choose:
     printf("Welcome To Department Creation Page\n\n");
     printf("\t1 - Add a New Department\n");
     printf("\t2 - Exit to Main Screen\n\n");
@@ -74,58 +86,117 @@ void dep_reg_screen(){
     if (choose == 2){
         title_screen();
     }    
-    else if (choose == 1){
+    else if (choose != 1){
+        printf("Wrong Entry!!!");
+        delay(0.6);
         system("cls");
-        user:
-        printf("Department Creation\n\n");
-        printf("NOTE: Username Shouldn't contain any space\n\n");
-        printf("Enter the Username for Department: ");
-        scanf("%s%*c", &username);
-
-        pass:
-        printf("\nPassword Must Be Between 5 And 15 Character");
-        printf("\nEnter Password: ");
-        scanf("%s%*c", &password);
-        if (strlen(password)<5){
-            printf("\nPassword too Short, Try Again!!!");
-            delay(1);
-            goto pass;
-        }
-        
-        FILE *dup = fopen("Dep_U_P.bin", "a+");
-        fprintf(dup, "%s %s", username, password);
-        fclose(dup);
-
-        printf("\nAccount Created!!!");
-        printf("\nFill Up The Remaining Details");
-        printf("\n\nNOTE: No Spaces, Use '-' Instead");
-
-        printf("\n\nEnter the Name of Department: ");
-        scanf("%s%*c", &d_name);
-        strupr(d_name);
-        
-        printf("Enter the Established Date (In Format mm-dd-yyyy): ");
-        scanf("%.2d-%.2d-%.4d", &e_m, &e_d, &e_y);
-
-        printf("Enter The Affiliated University: ");
-        scanf("%s%*c", &d_uni);
-        strupr(d_uni);
-
-        printf("Enter the name of HOD of the Dep:[Can Use Space In This]");
-        scanf("%[^\n]%*c", &d_hod);
-        strupr(d_hod);
-        
-        FILE *dd = fopen("dep_us_pw.bin", "a+");
-        fprintf (dd, "%s %.2d%.2d%.4d %s %s", d_name, e_m, e_d, e_y, d_uni, d_hod);
-        fclose(dd);
-
-        printf("\nDepartment Account Created..");
-        printf("\nReturning to Main Screen");  
-        delay(1);
-        system("cls");
-        title_screen();      
-
+        goto dep_create_choose;
     }
+
+    system("cls");
+
+  /** Department Username Password **/
+    user:
+    FILE *dcheck;
+    dcheck = fopen("DUP.dat", "r");
+ 
+    printf("\tDepartment Creation!!!\n\n");
+    printf("NOTE: Username Shouldn't contain any space, 5 - 10 Character\n\n");
+    printf("Enter the Username for Department: ");
+    scanf("%s%*c", &dep.un);
+    depch.un[10] = '\0';
+    depch.pw[15] = '\0';
+
+    while (fscanf(dcheck, "%s %s\n", depch.un, depch.pw) != EOF){
+        if (strcmp(dep.un, depch.un) == 0){
+            printf("\nUsername Already Taken!!!");
+            printf("\nTry Again with Another USERNAME");
+            delay(1);
+            system("cls");
+            goto user;
+        }
+    }
+
+    pass:
+    printf("\nPassword Must Be Between 5 And 15 Character");
+    printf("\nEnter Password: ");
+    scanf("%s%*c", dep.pw);
+    if (strlen(dep.pw)<5){
+        printf("\nPassword too Short, Try Again!!!");
+        delay(1);
+        goto pass;
+    }
+    fclose(dcheck);
+    
+    FILE *dup;
+    dup = fopen("DUP.dat", "a+");
+    fprintf(dup, "%s %s\n", dep.un, dep.pw);
+    fclose(dup);
+
+    printf("\nAccount Created!!!");
+    printf("\nFill Up The Remaining Details");
+    printf("\n\nNOTE: No Spaces, Use '-' Instead");
+
+  /** DEPARTMENT FURTHER DETAILS **/
+    printf("\n\nEnter the Name of Department: ");
+    scanf("%s%*c", &dep.dname);
+    strupr(dep.dname);
+    
+    printf("Enter the Established Date (In Format mm/dd/yyyy): ");
+    scanf("%d/%d/%d%*c", &dep.em, &dep.ed, &dep.ey);
+
+    printf("Enter The Affiliated University: ");
+    scanf("%s%*c", &dep.uni);
+    strupr(dep.uni);
+
+    printf("Enter the name of HOD of the Dep [Can Use Space In This]:\n");
+    scanf("%[^\n]%*c", &dep.hod);
+    strupr(dep.hod);
+    
+    FILE *dd;
+    dd = fopen("dep_details.dat", "a+");
+    fprintf (dd, "%s %s %.2d%.2d%.4d %s %s\n", dep.un, dep.dname, dep.em, dep.ed, dep.ey, dep.uni, dep.hod);
+    fclose(dd);
+
+    printf("\nDepartment Account Created..");
+    printf("\nReturning to Main Screen");  
+    delay(1);
+    system("cls");
+    title_screen();      
+
+}
+
+void department_screen(){
+
+    int flag = 0;
+    system("cls");
+    printf("\tDepartment Login!!!!");
+    printf("\n\nEnter Username: ");
+    scanf("%s%*c", dep.un);
+    printf("Enter Password: ");
+    scanf("%s%*c", dep.pw);
+
+    FILE *deptche;
+    deptche = fopen("DUP.dat", "r");
+    while(fscanf(deptche, "%s %s\n", depch.un, depch.pw) != EOF){
+        if(strcmp(dep.un, depch.un)==0 && strcmp(dep.pw, depch.pw)==0){
+            flag = 1;
+            system("cls");
+            printf ("Welcome %s", dep.un);
+            delay(1);
+            system("cls");
+            goto success;
+        } 
+    }
+    if(flag == 0){
+        printf("Username/Password Incorrect!!!");
+        delay(1);
+        department_screen();
+    } 
+
+    success:
+    printf("EOD");
+    delay(1);  
 }
 
 void delay(float sec) 
